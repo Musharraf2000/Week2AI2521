f787927db7812c251d9218571011f4bdb6659fe4
Musharraf2000 <musharraf.azhar19@gmail.com>
2026-03-16 09:17:29 +0530

    Created Person Class

---
+++ Week2AI2521/Person.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week2AI2521
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public string Name { get => name; set => name = value; }
+        public int Age { get => age; set => age = value; }
+
+        public void loginToLMS()
+        {
+            Console.WriteLine($"{name} logged in to LMS.");
+        }
+    }
+}
--- Week2AI2521/Program.cs
+++ Week2AI2521/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
+
+using Week2AI2521;
+
+Person p1 = new Person("Alice", 30);
+
+p1.Name = "Bob";
+p1.Age = 23;
+
+Console.WriteLine(p1.Name);
+
